f53eba3c0b6b77097ac3302ce7496d121840fb83
ExgleGaming <EaglesFan91x@outlook.com>
2026-05-23 01:02:19 -0400

    imgui setup

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,29 @@ int main()
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "window name");
 
+#pragma region imgui
 	rlImGuiSetup(true);
+
+	ImGuiIO &io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.FontGlobalScale = 2.5; // You can make it even bigger
+#pragma endregion
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
+	#pragma region imgui
 		rlImGuiBegin();
 
+		// Docking stuff for ImGui...
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		ImGui::PopStyleColor(2);
+	#pragma endregion
+	
 		Color c;
 		c.r = 255;
 		c.g = 0;
@@ -26,13 +40,35 @@ int main()
 
 		DrawText("Congrats! You created your first window!", 190, 200, 20, c);
 
+	#pragma region imgui windows
 		ImGui::Begin("test");
 
+		ImGui::ShowDemoWindow();
+
 		ImGui::Text("hello");
-		ImGui::Button("button");
+		if (ImGui::Button("button"))
+		{
+			std::cout << "Text\n";
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Button2"))
+		{
+			std::cout << "Second Button\n";
+		}
 
 		ImGui::End();
-		
+
+		ImGui::Begin("Second Window");
+
+		ImGui::Text("Hello");
+		ImGui::Separator();
+		ImGui::NewLine();
+		static float a = 0;
+		ImGui::SliderFloat("Slider", &a, 0, 1);
+
+		ImGui::End();
+	#pragma endregion
+
 		rlImGuiEnd();
 		EndDrawing();
 	}
